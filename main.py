587b7ef5e8eccc1554588296de24b808d84de700
Reuben Cleetus <rcleetus@gmail.com>
2021-11-01 19:26:30 -0400

    Updated port

--- main.py
+++ main.py
@@ -57,5 +57,4 @@ def get_quote_with_sentiment():
     return sentiment
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
-    pass
+    uvicorn.run(app, host="0.0.0.0", port=8000)
